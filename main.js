cb3033fa85fa47a488d3bde8590cc4c0a012c660
AnyaNecoder <ap.nerpina@gmail.com>
2024-10-29 16:01:00 +0600

    add section equipment

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-import './src/assets/global-style/style.scss'
+import './src/assets/global-style/style.scss';
+import { Equipment } from './src/components/Equipment/equipment';
 
 
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    Equipment();
 
 })
 
---
+++ src/components/Equipment/equipment.js
@@ -0,0 +1,22 @@
+export const Equipment = () => {
+    document.querySelector('.equipment__video-play-button').addEventListener('click', function() {
+        const videoWrapper = document.querySelector('.equipment__video-wrapper');
+        const youtubeUrl = 'https://rutube.ru/play/embed/baf5cfaff1b356f475f64692f8c7c213/';
+        
+        
+        const iframe = document.createElement('iframe');
+        iframe.setAttribute('width', '360');
+        iframe.setAttribute('height', '230');
+        iframe.setAttribute('src', youtubeUrl);
+        iframe.setAttribute('frameBorder', '0');
+        iframe.setAttribute('allow', 'clipboard-write; autoplay');
+        iframe.setAttribute('allowfullScreen', true);
+      
+        
+        videoWrapper.innerHTML = '';
+        videoWrapper.appendChild(iframe);
+      });
+}
+
+
+
